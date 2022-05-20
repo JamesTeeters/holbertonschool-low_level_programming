9bfd76611b7952c2baabcafc8cc2d85b9e7cf668
JamesTeeters <4925@holbertonschool.com>
2022-05-20 12:17:51 -0700

    'test'

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,6 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of a int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	iprintf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	return (0);
 }
