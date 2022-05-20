1ddb27a001c17e570ad13fcc99997145fdb5ffb0
JamesTeeters <4925@holbertonschool.com>
2022-05-20 10:37:49 -0700

    ''

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#includ <studio.h>
+/**
+ * put - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main () {
+	char str1[50];
+	 
+	strcpy(str1, ""Programming is like building a multilingual puzzle");
+	
+	puts(str1);
+
+	return 0;
+}
