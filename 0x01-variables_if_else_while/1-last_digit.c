2070cee9ccd0e79252ccde64822470f248022bbf
JamesTeeters <4925@holbertonschool.com>
2022-05-23 10:32:30 -0700

    'now allows neg num'

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,14 +14,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-		x = abs(n) % 10;
 	if (x > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, x);
 	}
 	if (x == 0)
 	{
-		printf("Lat digit of %d is %d and is 0\n", n, x);
+		printf("Last digit of %d is %d and is 0\n", n, x);
 	}
 	if (x < 6 && x != 0)
 	{
