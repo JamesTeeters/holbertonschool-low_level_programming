d80434f4d66b3d469baf213814bb7c524ba82718
JamesTeeters <4925@holbertonschool.com>
2022-05-23 09:30:53 -0700

    'checks the abs of last digit'

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,17 +12,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+		int x;
+
+		x = abs(n) % 10;
 	if (n > 5)
 	{
-		printf("Last digit of %d is and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
 	}
 	if (n == 0)
 	{
-		printf("Lat digit of %d is and is 0\n", n);
+		printf("Lat digit of %d is %d and is 0\n", n, x);
 	}
 	if (n < 6 && n != 0)
 	{
-		printf("Last digit of %d is and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
 
 	return (0);
