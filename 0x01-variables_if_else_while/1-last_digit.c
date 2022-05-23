b731207f7592ba9e599641844f2fc5bcf6b79561
JamesTeeters <4925@holbertonschool.com>
2022-05-23 10:18:22 -0700

    'altered variable "n" to var "x"'

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 		x = abs(n) % 10;
-	if (n > 5)
+	if (x > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, x);
 	}
-	if (n == 0)
+	if (x == 0)
 	{
 		printf("Lat digit of %d is %d and is 0\n", n, x);
 	}
-	if (n < 6 && n != 0)
+	if (x < 6 && x != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
