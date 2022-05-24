44bc747a1514fbcada7f04745017e4c43b9024c6
JamesTeeters <4925@holbertonschool.com>
2022-05-24 13:58:16 -0700

    'isalpha function'

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * _isalpha - Entry point
+ *
+ * @c: is input
  *
  * Return: Always 0 (Success)
  */
 	int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A'  && c  <= 'Z')
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	if (c >= 65  && c  <= 90)
 	{
 		return (1);
 	}
