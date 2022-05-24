3af8a1514ba218425103da680804b7a5b06251e9
JamesTeeters <4925@holbertonschool.com>
2022-05-24 04:03:10 -0700

    'use functions to print alphabet 10 times'

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * print_alphabet - Entry Point
  *
- * main - check the code
- *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-
-	return (0);
-}
 void print_alphabet(void)
 {
 	int x;
 
 	for (x = 'a'; x <= 'z'; x++)
 	{
-		putchar(x);
+		_putchar(x);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
 void print_alphabet_x10(void)
 {
 	int y;
