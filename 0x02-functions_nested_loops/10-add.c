58893aa25944738f9917ceefc4c288d7a821c97f
JamesTeeters <4925@holbertonschool.com>
2022-05-24 14:18:25 -0700

    'amde file executible and fixed betty comments'

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,9 +6,5 @@
  * Return: Always 0 (Success)
  */
 int add(int a, int b)
-{
-	int sum;
-	sum = a + b;
-	_putchar(sum);
     return (0);
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,10 @@
 #include "main.h"
 /**
- * main - Entry point
+ * jack_bauer - Entry point
  *
  * Return: Always 0 (Success)
  */
 void jack_bauer(void)
 {
-	int h, m;
 
-	h = '0';
-	m = '0';
-
-	while (h <= 24 &&
-    return (0);
 }
