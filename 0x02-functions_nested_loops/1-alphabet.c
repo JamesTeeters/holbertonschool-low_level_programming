1141154e5076a8fe8ca4e877dd712b3bd77abaf7
JamesTeeters <4925@holbertonschool.com>
2022-05-24 00:03:37 -0700

    'made function to print alphabet'

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * print_alphabet - Entry Point
+ *
+ * main - check the code
  *
  * Return: Always 0 (Success)
  */
+void print_alphabet(void);
 int main(void)
 {
-    return (0);
+	print_alphabet();
+
+	return (0);
+}
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
 }
