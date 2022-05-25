4fa29620d87973cbc977f3cb2dafc8632c3a004a
JamesTeeters <4925@holbertonschool.com>
2022-05-25 16:23:41 -0700

    'made times table'

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,4 +6,35 @@
  */
 void times_table(void)
 {
+	int c;
+	int r;
+	int p;
+
+	for (r = '0'; r < 10; r++)
+	{
+		for (c = '0'; c < 10; c++)
+		{
+			_putchar(c + 48);
+			_putchar(44);
+			_putchar(32);
+			_putchar(32);
+		}
+		p = r * c;
+
+		_putchar(10);
+		if (p < 10)
+		{
+			_putchar(p + 48);
+			_putchar(44);
+			_putchar(32);
+			_putchar(32);
+		}
+		else
+		{
+			_putchar(p / 10 + 48);
+			_putchar(p % 10 + 48);
+			_putchar(44);
+			_putchar(32);
+		}
+	}
 }
