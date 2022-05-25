c7100450c1fb04bb749acb8f7c9ce837aa7afdbe
JamesTeeters <4925@holbertonschool.com>
2022-05-25 14:11:23 -0700

    'write clock'

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,5 +6,38 @@
  */
 void jack_bauer(void)
 {
+	int h;
+	int m;
 
+	while (h <= '24')
+	{
+		if (h < '9')
+		{
+			_putchar('0');
+			_putchar(h + '0');
+		}
+		if (h > 9)
+		{
+		_putchar(h / 10 + '0');
+		_putchar(h % 10 + '0');
+		}
+		_putchar('\n');
+		h++;
+		_putchar(58);
+		while (m < '60')
+		{
+			if (m < 9)
+			{
+				_putchar('0');
+				_putchar(m + '0');
+			}
+			if (m > 9)
+			{
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
+			}
+			_putchar('\n');
+			m++;
+		}
+	}
 }
