08c1cf0605b0e761c49b3f26b91890099a499849
JamesTeeters <4925@holbertonschool.com>
2022-05-25 20:01:22 -0700

    'pls work'

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,5 +9,17 @@
  */
 void print_to_98(int n)
 {
-	putchar(n);
+	if (n != 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
 }
