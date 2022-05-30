fc182a0ad10fafe761b46acc55936d85bd7bc745
JamesTeeters <4925@holbertonschool.com>
2022-05-30 12:28:48 -0700

    'fixed betty error'

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
-/**
- * while - block out infinite loop
- *	while (i < 10)
- *	{
- *		putchar(i);
- *	}
- */
+	/**
+	* while (i < 10)
+	* {
+	*	putchar(i);
+	* }
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
