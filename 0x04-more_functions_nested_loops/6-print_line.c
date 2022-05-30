8c03ed30f0a54e726f28f6a3c5e5165f548abf6f
JamesTeeters <4925@holbertonschool.com>
2022-05-30 13:48:56 -0700

    'draw a line in the sand'

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,11 +2,20 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
- *
+ * print_line - draw a line in term
+ * @n: is input for how long the line should be
  * Return: Always 0.
  */
-int main(void)
+void print_line(int n)
 {
-return (0);
+	int l;
+
+	if (n >= 0)
+	{	
+		for (l = 0; l <= n; l++)
+		{
+			_putchar (95);
+		}
+	}
+	_putchar('\n');
 }
