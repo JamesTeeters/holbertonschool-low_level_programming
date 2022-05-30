15537d662ecfba6f23219a1536bf66c6f9fb6057
JamesTeeters <4925@holbertonschool.com>
2022-05-30 08:20:50 -0700

    'fixed line count'

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,22 +7,17 @@
 
 int main(void)
 {
-
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 /**
  * while - block out infinite loop
  *	while (i < 10)
  *	{
- *
  *		putchar(i);
- *
  *	}
  */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
