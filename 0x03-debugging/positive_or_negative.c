a052195c1d5cfda569a076e997e444ac2a66305a
JamesTeeters <4925@holbertonschool.com>
2022-05-30 07:50:44 -0700

    'debbuging positive_or_negative'

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+}
