0fe1c117dc2823b8a389b3341c3b94d546868ce8
JamesTeeters <4925@holbertonschool.com>
2022-05-31 12:15:24 -0700

    'classic FizzBuzz'

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,11 +2,32 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - print Fizz for multiples of 3, Buzz for multiples of 5, and FizzBuzz for both
  *
  * Return: Always 0.
  */
 int main(void)
 {
-return (0);
+	int num;
+
+	for (num = 1; num < 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz\n");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz\n");	
+		}
+		else
+		{
+		printf("%d\n", num);
+		}
+	}
+	return (0);
 }
