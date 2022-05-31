68180f485ed866b65aa0531ca07d807f2f69144a
JamesTeeters <4925@holbertonschool.com>
2022-05-31 10:19:42 -0700

    'print a diagonal line in the terminal'

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,11 +2,26 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
- *
+ * print_diagonal - print a diagonal line in term
+ * @n: how long the line will be
  * Return: Always 0.
  */
-int main(void)
+void print_diagonal(int n)
 {
-return (0);
+	int d;
+	int s;
+
+	if (n > 0)
+	{
+		for (d = 0; d < n; d++)
+		{
+			for (s = 0; s < d; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
 }
