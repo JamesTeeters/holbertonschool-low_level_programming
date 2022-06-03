a1287cffa0fd5e5a5d78e46b7ba5d507eea79ef9
JamesTeeters <4925@holbertonschool.com>
2022-06-03 10:38:42 -0700

    'print every other num in string'

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,22 @@
  */
 void rev_string(char *s)
 {
-	_putchar(*s);
+	int p;
+	int x;
+	char tmp;
+
+	while (s[p] != '\0')
+	{
+		p++;
+	}
+	p = p - 1;
+	for (x = 0; x <= p; x++)
+	{
+		p--;
+		tmp = s[p];
+		s[p] = s[x];
+		s[x] = tmp;
+	}
+	*s = s[x];
+
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
- * puts2 - print every other character
+ * puts2 - check code
  *
  * @str: interger input
  *
  */
 void puts2(char *str)
 {
-	_putchar(*str);
+	int p = 0;
+
+	while (str[p] != '\0')
+	{
+		if (str[p] % 2 == 0)
+		{
+			_putchar(str[p]);
+		}
+		p++;
+	}
+	_putchar('\n');
 }
