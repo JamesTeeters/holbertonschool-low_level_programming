148d8ca6f0c878667c4bec154bedc8bfa732891d
JamesTeeters <4925@holbertonschool.com>
2022-06-07 11:43:43 -0700

    'reverse an array of intergers'

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,18 @@
 #include "holberton.h"
 
 /**
- * p
+ * reverse_array - reverse an array of intergers
+ * @a: array
+ * @n: length
  */
-
+void reverse_array(int *a, int n)
 {
+	int i, tmp;
 
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+	}
 }
