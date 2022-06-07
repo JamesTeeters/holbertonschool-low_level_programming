57b76e0191edb548d10c59603d0b83401cdaf1b5
JamesTeeters <4925@holbertonschool.com>
2022-06-07 11:26:40 -0700

    'compare 2 strings'

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,22 @@
 #include "holberton.h"
 
 /**
- * p
+ * _strcmp - compare 2 strings
+ *
+ * @s1: first input string
+ * @s2: second input string
+ *
+ * Return: less than greater than or equal
  */
-
+int _strcmp(char *s1, char *s2)
 {
+	int i = 0, j;
 
+	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
+	{
+		i++;
+	}
+	j = s1[i] - s2[i];
+
+	return (j);
 }
