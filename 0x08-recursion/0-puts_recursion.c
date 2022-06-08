be74e787803c6aa5df63cf26a7e331acc76ecd4f
JamesTeeters <4925@holbertonschool.com>
2022-06-08 15:30:59 -0700

    'created puts function'

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - create puts function
+ * @s: input string
+ *
+ */
+void _puts_recursion(char *s)
+{
+	int c;
+	
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
