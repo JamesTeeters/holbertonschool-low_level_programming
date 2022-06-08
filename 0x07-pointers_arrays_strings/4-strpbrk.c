e67e0f56263b6d095e341e6c789aaf45edcfeb10
JamesTeeters <4925@holbertonschool.com>
2022-06-07 19:26:21 -0700

    'betty'

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,9 +12,9 @@ char *_strpbrk(char *s, char *accept)
 	int i;
 
 	while (*s++)
-	
+
 	{
-		for (i = 0; accept[i] != '\0'; i++)	
+		for (i = 0; accept[i] != '\0'; i++)
 		{
 			if (*s == accept[i])
 			{
