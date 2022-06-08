ab194a31ec3a23f2532fbe9196a939926e50a532
JamesTeeters <4925@holbertonschool.com>
2022-06-07 20:11:10 -0700

    'test'

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,9 +10,32 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (;; haystack++)
-		for (;; needle++)
-			if (*needle == *haystack)
-				return (needle);
-	return ('\0');
+	 const char *n, *h;
+
+	n = needle;
+	if (*n == '\0')
+	{
+		return (haystack);
+	}
+	for (; *haystack != '\0'; haystack++)
+	{
+		if (*haystack != *n)
+		{
+			continue;
+		}
+		h = haystack;
+		while (1)
+		{
+			if (*n == '\0')
+			{
+				return (haystack);
+			}
+			if (*h++ != *n++)
+			{
+				break;
+			}
+		}
+		n = needle;
+	 }
+	 return ('\0');
 }
