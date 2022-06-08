23b0b5e2eaee6c0ec225e0697547c1a7687a7d5d
JamesTeeters <4925@holbertonschool.com>
2022-06-07 18:07:29 -0700

    'fixed loops'

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,17 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int x, y;
+	unsigned int x, y;
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (y = 0; accept[x] != '\n'; y++)
+		for (y = 0; accept[y] != '\0'; y++)
 		{
 			if (accept[y] == s[x])
 				break;
 		}
 		if (accept[y] == '\0')
-			return (x);
+			break;
 	}
-	return (0);
+	return (x);
 }
