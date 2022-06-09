851a0ed2a405d4f3e98245783c9666a4a02a6bd5
JamesTeeters <4925@holbertonschool.com>
2022-06-08 17:18:05 -0700

    'reverse a string using recursion'

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
 /**
- *
+ * _print_rev_recursion - print a string in reverse
+ * @s: input string
  *
  */
-
+void _print_rev_recursion(char *s)
 {
-
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+			_putchar(*s);
+	}
+	else
+		return;
 }
