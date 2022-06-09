60b616f6d06ce631f45e3a7a807a9b993da5a1e5
JamesTeeters <4925@holbertonschool.com>
2022-06-09 13:08:44 -0700

    'test'

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,24 @@
 #include "holberton.h"
 
 /**
- * p
+ * cap_string - capilalize all words in a string
+ *
  */
-
+char *cap_string(char *s)
 {
+	int i;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ')
+		{
+			i++;
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+
+		}
+	}
+	return (s);
 }
