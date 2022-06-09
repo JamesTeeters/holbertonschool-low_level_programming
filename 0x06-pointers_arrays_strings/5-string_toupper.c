6d508ae9424cf199506408873028a9b14ded3dea
JamesTeeters <4925@holbertonschool.com>
2022-06-09 11:51:03 -0700

    'fixed loop to iterate var i'

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,12 +12,12 @@ char *string_toupper(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 'A' && s[i] <= 'Z')
-			return (s);
+		/*if (s[i] >= 'A' && s[i] <= 'Z')
+			return (s);*/
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			*s = s[i] - 32;
+			s[i] = s[i] - 32;
 		}
 	}
-	return (s - 1);
+	return (s);
 }
