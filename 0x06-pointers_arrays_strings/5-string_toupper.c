99f43ee355923e6ab5315795bcab13c7a0eb3f56
JamesTeeters <4925@holbertonschool.com>
2022-06-09 10:57:57 -0700

    'toupper'

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,23 @@
 #include "holberton.h"
 
 /**
- * p
+ * string_toupper - change all lowercase letters to upper case.
+ *@s: input string
+ *
+ * Return: uppercase string
  */
-
+char *string_toupper(char *s)
 {
+	int i;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			return (s);
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			*s = s[i] - 32;
+		}
+	}
+	return (s);
 }
