0a86e927a5df9c3a3de8718d32c962156498c124
JamesTeeters <4925@holbertonschool.com>
2022-06-08 18:20:40 -0700

    'test'

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
+ * _sqrt_recursion - return the suare root of a given number
+ * @n: input interger
  *
- *
+ * Return: value
  */
-
+int _sqrt_recursion(int n)
 {
-
+	return (n);
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *
- *
+ * is_prime_number - return prime numbers
+ * @n: input interger
+ * Return: prime number
  */
-
+int is_prime_number(int n)
 {
-
+	return (n);
 }
