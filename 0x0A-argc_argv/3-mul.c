d986e2b521d136444b466c9f0d495db744cefba3
JamesTeeters <4925@holbertonschool.com>
2022-06-14 14:22:31 -0700

    'multiply two numbers using argv'

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * main - check code
@@ -12,5 +13,18 @@
 
 int main(int argc, char *argv[])
 {
+	int result;
+
+	if (argc == 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+			printf("%d\n", result);
+	}
+	else
+	{	
+		printf("Error\n");
+		return (1);
+	}
+
 	return (0);
 }
