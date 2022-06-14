18fb65c1e8ce15876b9c86fc4b7b50d151080916
JamesTeeters <4925@holbertonschool.com>
2022-06-13 17:38:02 -0700

    'make dynamic library'

---
+++ 0x0A-dynamic_libraries/100_atoi.c
@@ -0,0 +1,17 @@
+# include "main.h"
+
+/**
+ * _atoi - stuff
+ *
+ * @s: input string
+ *
+ * Return: interger
+ */
+
+int _atoi(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		return(i);
+}
