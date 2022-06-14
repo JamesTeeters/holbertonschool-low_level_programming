1fbc7a1e52258fe94e103b0d864c5a1d23942f1e
JamesTeeters <4925@holbertonschool.com>
2022-06-14 15:02:25 -0700

    'produce sum of givent intergers'

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * main - check code
@@ -12,5 +13,26 @@
 
 int main(int argc, char *argv[])
 {
+	int x, y, sum;
+
+	sum = 0;
+	if (argc <= 1)
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (argv[x][y] < '0' || argv[x][y] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[x]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
