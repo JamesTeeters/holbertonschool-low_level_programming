754c8fe24bf03a2a95935651ed1bb499d0091149
JamesTeeters <4925@holbertonschool.com>
2022-06-14 13:07:46 -0700

    'tell the name of the command'

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * main - check code
@@ -10,5 +12,11 @@
 
 int main(int argc, char *argv[])
 {
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
