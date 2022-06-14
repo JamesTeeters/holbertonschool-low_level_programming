de411404a6525a0e727d46117706c9112f176077
JamesTeeters <4925@holbertonschool.com>
2022-06-14 14:00:32 -0700

    'print number of arguments (nargs)'

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * main - check code
@@ -10,5 +13,13 @@
 
 int main(int argc, char *argv[])
 {
+	int i;
+	(void)argv;
+
+	i = 0;
+	while (i < argc)
+		i++;
+	i = i - 1;
+	printf("%d\n", i);
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * main - check code
