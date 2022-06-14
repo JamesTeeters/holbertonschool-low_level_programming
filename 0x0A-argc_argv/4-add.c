f7ca6e80c6918b1734d9f2d92b4acedfe6e2dd1e
JamesTeeters <4925@holbertonschool.com>
2022-06-14 11:50:25 -0700

    'created files'

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check code
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* prototype functions */
+
+int _putchar(char c);
+/* end of prototypes */
+
+#endif /*MAIN_H*/
