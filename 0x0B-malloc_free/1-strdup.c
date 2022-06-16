d18060c6badd99ca4f6364cf291fbc0b5a8afda1
JamesTeeters <4925@holbertonschool.com>
2022-06-16 15:19:19 -0700

    'duplicated a string'

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _strdup - returns a pointer of a string using malloc
+ * _strdup - duplicat a string using malloc
  * @str: input string
  *
  * Return: character pointer or NULL
@@ -13,16 +13,16 @@ char *_strdup(char *str)
 	char *ptr;
 	unsigned int i, len;
 
-	if (str == 0)
+	if (str == NULL)
 		return (NULL);
 
 	len = 0;
 	while (str[len])
-		len++
+		len++;
 
-	ptr = (char *)malloc(size of(char) * (len + 1));
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
 
-	if (ptr == 0)
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < len; i++)
