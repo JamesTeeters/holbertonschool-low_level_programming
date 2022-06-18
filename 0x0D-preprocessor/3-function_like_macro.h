48668abe2a090c71cc6fe904a16a39a4960e01de
JamesTeeters <4925@holbertonschool.com>
2022-06-17 20:42:48 -0700

    'test'

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 /*end of include guard*/
-#define ABS(X)((X < 0) ? (X - 1) : (X))
+#define ABS(X)((X < 0) ? (X) * (-1) : (X))
 
 #endif /*FUNCTION_LIKE_MACRO_H*/
