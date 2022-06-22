9dae5e716394ca63c512646305a703776414502a
JamesTeeters <4925@holbertonschool.com>
2022-06-22 12:36:24 -0700

    'get new dog'

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,11 +1,44 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- * init_dog - dog
- * @d: input structure
+ * new_dog - dog
  * @name: dog name
  * @age: dog age
  * @owner: owner name
  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *n;
+	char *o;
+	dog_t *ptr;
 
+	n = malloc(sizeof(name));
+	if (n == NULL)
+	{
+		return (NULL);
+		free(n);
+	}
+	o = malloc(sizeof(owner));
+	if (o == NULL)
+	{
+		return (NULL);
+		free(o);
+	}
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+		free(ptr);
+	}
+	
+	strcpy(n, name);
+	strcpy(o, owner);
+	
+	ptr->name = n;
+	ptr->age = age;
+	ptr->owner = o;
+
+	return (ptr);
+}
