9e4b015e8d7409dfe090e4d3f8325be59c0394ad
JamesTeeters <4925@holbertonschool.com>
2022-06-22 12:09:51 -0700

    'added typedef'

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,14 +7,16 @@
  * @age: dogs age
  * @owner: dogs owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 /*end of structure*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 /*end of prototypes*/
 #endif/*DOG_H*/
