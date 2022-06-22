03bfda254a7b2d27fe6a612fff2ed76fd9eb45df
JamesTeeters <4925@holbertonschool.com>
2022-06-22 10:38:30 -0700

    'added struct comment'

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 /*end of include guard*/
+/**
+ * struct dog - create a dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
 struct dog
 {
 	char *name;
