7d7c64c7e542b293516663ce4350950e652ea0a6
JamesTeeters <4925@holbertonschool.com>
2022-06-22 13:41:46 -0700

    'test'

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,9 +1,15 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * free_dog - dogs will be free
+ * @d: dogs to be freed
+ */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
-		return (NULL);
+		return;
+	free(d->name);
+	free(d->owner);
 	free(d);
 }
