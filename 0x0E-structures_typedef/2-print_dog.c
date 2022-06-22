97cb5a59f108a4d9b34fb72fba9530d90dc20720
JamesTeeters <4925@holbertonschool.com>
2022-06-22 11:58:37 -0700

    'syntax error'

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age != d->age)
-		printf("(nil)\n");
+		printf("(nil)"\n);
 	else
 		printf("age: %f\n", d->age);
 	if (d->owner == NULL)
