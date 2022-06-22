52cc613f28ba76e66410c83bc69ac17f93448d1c
JamesTeeters <4925@holbertonschool.com>
2022-06-22 12:40:07 -0700

    'betty'

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * new_dog - dog
  * @name: dog name
  * @age: dog age
  * @owner: owner name
+ *
+ * Return: struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -32,10 +35,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 		free(ptr);
 	}
-	
+
 	strcpy(n, name);
 	strcpy(o, owner);
-	
+
 	ptr->name = n;
 	ptr->age = age;
 	ptr->owner = o;
