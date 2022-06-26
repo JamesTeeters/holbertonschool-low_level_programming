bbfead5f399e833a9abf7c2bf5ba0213f5ce674f
JamesTeeters <4925@holbertonschool.com>
2022-06-26 07:50:50 -0700

    'test'

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,11 +20,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	i = 0;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	while (ops[i].op != NULL)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
+		i++
 	}
 
 	printf("Error\n");
