9d5ec432509ead7a38c5deac3ec0e1c51283f56a
JamesTeeters <4925@holbertonschool.com>
2022-06-26 07:40:31 -0700

    'betty fix'

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,6 +16,6 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]),atoi(argv[3])));
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
