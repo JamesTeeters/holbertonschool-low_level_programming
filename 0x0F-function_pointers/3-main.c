47f0b91a5e053d0a558960263e7ef410330353af
JamesTeeters <4925@holbertonschool.com>
2022-06-26 07:37:14 -0700

    added main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,21 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - run calculator
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]),atoi(argv[3])));
+	return (0);
+}
