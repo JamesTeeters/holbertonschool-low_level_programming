8dd91c247401d858813609e03d0cbbc0400f236c
JamesTeeters <4925@holbertonschool.com>
2022-07-07 15:35:08 -0700

    ''

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_lists -  function that prints all the elements of a list
+ * @h: input list
+ *
+ * Return: number of elements in list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		
+		h = h->next;
+		count++;
+
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+0-Write a function that prints all the elements of a list_t list.
+1-Write a function that returns the number of elements in a linked list_t list.
+2-Write a function that adds a new node at the beginning of a list_t list.
+3-Write a function that adds a new node at the end of a list_t list.
+4-Write a function that frees a list_t list.
