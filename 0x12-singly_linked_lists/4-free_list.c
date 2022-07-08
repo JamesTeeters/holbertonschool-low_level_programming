ab2837c6dae396dca090a2dbd15018fed078770c
JamesTeeters <4925@holbertonschool.com>
2022-07-08 12:03:08 -0700

    'freed a list'

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,5 +1,19 @@
 #include "lists.h"
 /**
+ * free_list -  a function that frees a list.
+ * @head: first node in list.
  *
+ * Return: Nothing.
  */
+void free_list(list_t *head)
+{
+	list_t *current_node;
 
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
