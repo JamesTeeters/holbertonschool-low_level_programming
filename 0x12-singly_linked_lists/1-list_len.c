f389ee81ebec75492e71d1abee6961059492d475
JamesTeeters <4925@holbertonschool.com>
2022-07-08 09:08:09 -0700

    'return length of list'

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,18 @@
 #include "lists.h"
 /**
+ * list_len - a function that returns the number of elements in a linkedlist.
+ * @h: given list.
  *
+ * Return: length of list.
  */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
 
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
