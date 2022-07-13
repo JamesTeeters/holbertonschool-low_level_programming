5472f56f71e961ca4f2473dbd2c021b9b37caa05
JamesTeeters <4925@holbertonschool.com>
2022-07-13 12:44:43 -0700

    'created file 1'

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: input number.
+ *
+ * Return: Nothing.
+ */
+void print_binary(unsigned long int n)
+{
+
+}
