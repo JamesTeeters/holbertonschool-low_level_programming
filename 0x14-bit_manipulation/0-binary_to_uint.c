da1ba285cbb161e3db8a4ab919c367adc3547142
JamesTeeters <4925@holbertonschool.com>
2022-07-13 12:30:10 -0700

    ''

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,10 +11,10 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 	unsigned int total = 0;
 
-	while (b[i])
-	{
 		if (b == NULL)
 			return (0);
+	while (b[i])
+	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		if (b[i] == '1')
