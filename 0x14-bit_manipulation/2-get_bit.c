097b1e23a61b15c2004c4b1f5f15282ec16c8798
JamesTeeters <4925@holbertonschool.com>
2022-07-13 12:42:40 -0700

    'added file 2'

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: input number.
+ * @index: place to grab bit.
+ *
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+}
