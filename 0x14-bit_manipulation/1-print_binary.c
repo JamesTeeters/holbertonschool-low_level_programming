c35c2ea835892a6e499399473e57628b7f176041
JamesTeeters <4925@holbertonschool.com>
2022-07-14 11:58:34 -0700

    ''

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,5 +7,19 @@
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int cp;
+	int len = 0;
 
+	cp = n;
+
+	while ((cp >>= 1) > 0)
+		len++;
+
+	while (len >= 0)
+	{
+		if ((n >> len--) & 1)
+			_putchar('1');
+		else 
+			_putchar('0');
+	}
 }
