d0e3aa5997fef2c2ff9071c9727356f2ba72b077
JamesTeeters <4925@holbertonschool.com>
2022-07-14 17:58:31 -0700

    'made tmp_node a header'

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,15 +7,16 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t node; /*node to be deleted*/
+	listint_t *tmp_node; /*node to be deleted*/
 	int data; /*data to be returned*/
 
 	if (*head == NULL)
 		return (0);
-	node = *head; /*set node to head node*/
-	data = *node->data; /*store data from node*/
-	*head = node->next; /*move the haed to the next node*/
-	free(node); /*delete node*/
+
+	tmp_node = *head; /*set node to head node*/
+	data = tmp_node->data; /*store data from node*/
+	*head = tmp_node->next; /*move the haed to the next node*/
+	free(tmp_node); /*delete node*/
 
 	return (data);
 
