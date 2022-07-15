54ff39e3b20239808ea202fb52d0fea6bdbfe387
JamesTeeters <4925@holbertonschool.com>
2022-07-15 10:36:43 -0700

    'grab node at given idex'

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - function that returns the nth node of a list.
+ * @head: first node.
+ * #index: node to be grabed.
+ *
+ * Return: the nth node.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node = head; /* node to be grabbed */
+	unsigned int i;
+	
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; i < index; i++)
+	{
+		node = node->next;
+		if (node == NULL)
+			return (NULL);
+	}
+
+	return (node);
+}
