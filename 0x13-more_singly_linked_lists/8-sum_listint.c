282a57fcf12d8290876c45bcf6ef0b37f7d22d17
JamesTeeters <4925@holbertonschool.com>
2022-07-15 11:00:14 -0700

    ''

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,17 +1,19 @@
 #include "lists.h"
 /**
+ * sum_listint - sum of all data in list.
+ * @head: first node in list.
  *
- *
+ * Return: sum.
  */
 int sum_listint(listint_t *head)
 {
 	int i = 0;
 	listint_t *tmp_node = head;
-	
+
 	while (tmp_node != NULL)
 	{
 		i += tmp_node->n;
-		tmp_node = tmp_node->next;	
+		tmp_node = tmp_node->next;
 	}
 	return (i);
 }
