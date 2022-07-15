752ca792abc10b9a1ef7c1ec47adb635e343b72e
JamesTeeters <4925@holbertonschool.com>
2022-07-14 17:12:14 -0700

    ''

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,9 +13,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > 64)
 		return (-1);
 
-	bit = 0 << index;
+	bit = 1 << index;
 
-	*n = (*n | bit);
+	if (*n & bit)
+		*n ^= bit;
 
 	return (1);
 }
