ca0fa8dd0f41f5fcde093d3d164c40745ad87509
JamesTeeters <4925@holbertonschool.com>
2022-07-18 15:24:48 -0700

    'created file'

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ *
+ * @filename: name of file.
+ * @text_content: contents of file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, out;
+	unsigned int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	len = strlen(text_content);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR);
+	out = write(fd, text_content, len);
+
+	if (fd < 0 || out < 0)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
