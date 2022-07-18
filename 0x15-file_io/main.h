9710af58cbc1df002f6d0d8062c21c78d2caeec4
JamesTeeters <4925@holbertonschool.com>
2022-07-18 14:49:15 -0700

    'updated includes'

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* end of include guard*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+/*end of includes*/
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+/*end of prototype functions*/
+
+#endif /*MAIN_H*/
