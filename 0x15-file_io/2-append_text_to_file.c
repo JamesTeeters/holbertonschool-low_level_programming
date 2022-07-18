1e3b3a14c3d949b7c153061a2e74d2a8a8bf4aac
JamesTeeters <4925@holbertonschool.com>
2022-07-18 15:45:52 -0700

    'fixed betty error'

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
