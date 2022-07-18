0a85fe5561c7d0c8cf31fb2af1ed8ec4189993ca
JamesTeeters <4925@holbertonschool.com>
2022-07-18 15:35:51 -0700

    'removed len var'

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,16 +10,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, out;
-	unsigned int len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
 
-	len = strlen(text_content);
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	out = write(fd, text_content, len);
+	out = write(fd, text_content, strlen(text_content));
 
 	if (fd < 0 || out < 0)
 		return (-1);
