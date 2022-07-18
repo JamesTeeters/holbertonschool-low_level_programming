15dd6a96ad2577158609fafea729e93d624c5675
JamesTeeters <4925@holbertonschool.com>
2022-07-18 16:54:55 -0700

    'fixed betty'

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,10 +14,13 @@ int  main(int argc, char *argv[])
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	
+
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from < 0)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to < 0)
@@ -26,7 +29,10 @@ int  main(int argc, char *argv[])
 	do {
 		rd_check = read(file_from, buff, 1024);
 		if (rd_check < 0)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",argv[1]), exit(98);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 
 		wr_check = write(file_to, buff, rd_check);
 		if (wr_check < 0)
@@ -37,6 +43,6 @@ int  main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 	if (close(file_to) < 0)
 		dprintf(STDERR_FILENO, "Error: Can;t close fd %d\n", file_to), exit(100);
-	
+
 	return (0);
 }
