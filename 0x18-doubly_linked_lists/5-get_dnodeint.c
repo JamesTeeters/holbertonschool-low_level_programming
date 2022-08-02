edc6027d96aa781f196f7d18374f097f03ea6b1c
JamesTeeters <4925@holbertonschool.com>
2022-08-02 13:51:44 -0700

    'added case for if head is NULL'

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -10,6 +10,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return(NULL);
+
 	while (head->next != NULL)
 	{
 		if (i == index)
