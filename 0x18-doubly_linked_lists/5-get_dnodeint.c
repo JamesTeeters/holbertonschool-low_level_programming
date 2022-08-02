7b7a25c4d3a903cbe33505f286d9c21fd4257b8c
JamesTeeters <4925@holbertonschool.com>
2022-08-02 13:49:17 -0700

    'moved loop before iterates'

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -12,10 +12,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	while (head->next != NULL)
 	{
-		head = head->next;
-		i++;
 		if (i == index)
 			return (head);
+		head = head->next;
+		i++;
 	}
 
 	return (NULL);
