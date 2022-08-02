5ae7283b9754c3d2f05a6aeab0a858808e525da6
JamesTeeters <4925@holbertonschool.com>
2022-08-02 12:42:34 -0700

    'check for malloc error'

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *newnode;/*initilize new node*/
 
 	newnode = malloc(sizeof(dlistint_t));/*allocate new node*/
+	if (newnode == NULL)
+		return (NULL);
 
 	newnode->n = n;/*store data in node*/
 
