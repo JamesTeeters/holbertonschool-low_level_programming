0e7fd79a512e42fb5e55225166aee52aac269350
JamesTeeters <4925@holbertonschool.com>
2022-08-05 07:57:26 -0700

    'fixed betty error'

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hash_table-create - function that creates a hash table
+ * hash_table_create - function that creates a hash table
  * @size: size of array.
  *
  * Return: A pointer to created hash table.
@@ -9,17 +9,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 	unsigned long int i;
-	
+
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
-	
+
 	table->size = size;
-	table->array = malloc(sizeof(hash_table_t*) * size);
+	table->array = malloc(sizeof(hash_table_t *) * size);
 	if (table->array == NULL)
 	{
 		free(table);
-		return(NULL);
+		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
